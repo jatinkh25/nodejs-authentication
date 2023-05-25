a9cd8fb898605f9231a123721c4994f7efc899f1
jatinkh25 <jatinkhatri25.jk@gmail.com>
2023-05-25 16:46:36 +0530

    login and signup templates, routes and controllers added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
 require('dotenv').config()
 
 const app = express()
@@ -21,3 +22,5 @@ mongoose
 // routes
 app.get('/', (req, res) => res.render('home'))
 app.get('/smoothies', (req, res) => res.render('smoothies'))
+
+app.use(authRoutes)
